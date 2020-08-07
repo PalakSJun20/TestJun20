18450aa74b74477a7d23ea2e3505846b53099ad7
Palak Soni <palaksoni1907@gmail.com>
2020-08-07 22:45:16 +0530

    2nd Branch add file

--- src/conflictscenario/Test.java
+++ src/conflictscenario/Test.java
@@ -4,5 +4,7 @@ public class Test {
 
 public static void main(String[] args) {
 		System.out.println("Test File In branch A");
+		System.out.println("Heleo In branch B");
+		System.out.println("Hello");
 	}
 }
