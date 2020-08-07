c14fc7df942e03d5488d0e5daa6959e2f8e7e088
Palak Soni <palaksoni1907@gmail.com>
2020-08-07 22:13:17 +0530

    Add B

--- src/p1/Test.java
+++ src/p1/Test.java
@@ -3,6 +3,7 @@ package p1;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("In Branch A");
-
+		System.out.println("Hello Branch B");
+		System.out.println("Bye Branch B");
 	}
 }
