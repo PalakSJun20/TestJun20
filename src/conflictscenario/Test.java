29d93df563179e1ec6924c5638204832eef961ae
Palak Soni <palaksoni1907@gmail.com>
2020-08-07 20:42:26 +0530

    CHanges

--- src/conflictscenario/Test.java
+++ src/conflictscenario/Test.java
@@ -3,7 +3,7 @@ package conflictscenario;
 public class Test {
 
 public static void main(String[] args) {
-		System.out.println("Test File");
-		System.out.println("Hello In Branch A");
+		System.out.println("Test File In Branch A");
+		System.out.println("Hello ");
 	}
 }
