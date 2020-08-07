f1c060fa1720aad9f880fcf92a30865416cc14d7
Palak Soni <palaksoni1907@gmail.com>
2020-08-07 20:36:53 +0530

    Test file added

--- src/conflictscenario/Test.java
+++ src/conflictscenario/Test.java
@@ -3,6 +3,7 @@ package conflictscenario;
 public class Test {
 
 public static void main(String[] args) {
-		System.out.println("Test File In branch A");
+		System.out.println("Test File");
+		System.out.println("Hello In Branch A");
 	}
 }
